b8c385b2e240891c46ca545f0c07be98bd187ad3
dbundi <63751950+dbundi@users.noreply.github.com>
2021-08-04 12:54:10 +0300

    Add files via upload

---
+++ R/ds.R
@@ -0,0 +1,15 @@
+#' Creates histogram, boxplot and numeric summary
+#' @export
+#' @param x numeric variable
+ds <- function(x){
+  # 1 row and 2 columns
+  par(mfrow=c(1,2))
+  #Histogram
+  hist(x, col=rainbow(30))
+  #Box plot
+  boxplot(x,col="green")
+  par(mfrow=c(1,1))
+  #Numerical Summary
+  data.frame(min=min(x),
+             mean=mean(x))
+}
